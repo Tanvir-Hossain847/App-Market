745f7d7ef39bacf8ae118caa2d46a1237ac5f385
Tanvir-Hossain847 <tanvirhossaintufa@gmail.com>
2025-10-09 18:17:05 +0600

    add button in error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import errorImg from '../../assets/error-404.png'
+import { Link } from 'react-router';
 
 const Error = () => {
     return (
@@ -10,6 +11,7 @@ const Error = () => {
             <div className="text-center">
                 <h1 className='text-5xl font-bold my-5'>Ooops! Page Not Found</h1>
                 <p className='text-gray-500 text-xl'>The Page You Are Looking For Is Not Found</p>
+                <Link to={'/'}><button className='btn bg-btn text-white mt-3'>Go Back</button></Link>
             </div>
         </div>
     );
